24df66bfbea10d650b4d1d8d49735c97bb920341
akeem lagundoye <akeemifedayolag@gmail.com>
2022-06-03 22:53:52 +0100

    Pagination, authentication classes

--- EDUQUEST/EDUQUEST/settings.py
+++ EDUQUEST/EDUQUEST/settings.py
@@ -130,3 +130,16 @@ STATIC_URL = '/static/'
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# Django-REST-Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- EDUQUEST/EDUQUEST/urls.py
+++ EDUQUEST/EDUQUEST/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django_registration.backends.one_step.views import RegistrationView
+from users.forms import CustomUserForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
