e435a09a01cc650c6444b8cc6413ce968286a167
akeem lagundoye <akeemifedayolag@gmail.com>
2022-06-03 22:05:37 +0100

    users model

--- EDUQUEST/users/models.py
+++ EDUQUEST/users/models.py
@@ -5,4 +5,6 @@ from django_countries.fields import CountryField
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
     location = CountryField()
