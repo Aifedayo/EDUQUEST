4530f0a3f51c39a74d7fadd3f0f590a92e74f3e5
akeem lagundoye <akeemifedayolag@gmail.com>
2022-06-04 19:40:04 +0100

    templates

--- EDUQUEST/eduquestapi/apps.py
+++ EDUQUEST/eduquestapi/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EduquestapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eduquestapi'
+
+    def ready(self):
+        import eduquestapi.signals
